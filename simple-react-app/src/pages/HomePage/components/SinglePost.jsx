7bf49ba4be0e879fb542b32e205cbc1ac8f40c30
borislavpachev <borislav.pachev@gmail.com>
2024-10-25 11:36:41 +0300

    style: better responsiveness on smaller screens added

--- simple-react-app/src/pages/HomePage/components/SinglePost.jsx
+++ simple-react-app/src/pages/HomePage/components/SinglePost.jsx
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 export default function SinglePost({ post }) {
   return (
     <div className="my-5 w-full text-left">
-      <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 justify-between items-center p-5 bg-slate-700 rounded-t-lg text-xl font-semibold border-b-2 border-orange-500">
-        <h4 className='uppercase'>{post.title}</h4>
+      <div className="flex flex-col md:flex-row text-base md:text-lg space-y-5 md:space-y-0 justify-between items-center p-5 bg-slate-700 rounded-t-lg font-semibold border-b-2 border-orange-500">
+        <h4 className="uppercase">{post.title}</h4>
         <p>User: {post.userId}</p>
       </div>
-      <p className='bg-orange-500/50 text-lg px-5 py-10 italic rounded-b-lg'>{post.body}</p>
-  
+      <p className="bg-orange-500/50 px-5 py-10 italic rounded-b-lg">
+        {post.body}
+      </p>
     </div>
   );
 }
