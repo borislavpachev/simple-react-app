9adf989d9415dbf6083ea52dfdc49b7c2ae2204b
borislavpachev <borislav.pachev@gmail.com>
2024-10-25 10:41:02 +0300

    chore: Absolute imports in use

--- simple-react-app/vite.config.js
+++ simple-react-app/vite.config.js
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {},
+  resolve: { alias: [{ find: '@', replacement: resolve('src') }] },
 });
